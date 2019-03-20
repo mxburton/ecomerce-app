08dca6d82ef4c6bea6e5afce849218bf0d6530f2
gaserd <gaserd123@gmail.com>
2019-03-20 11:30:38 +0300

    Updates

---
+++ precache-manifest.b2a2a3a6f99317f963d209e5287c6ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4188da6b21417c0d6b",
+    "url": "/ecomerce-app/static/js/runtime~main.b818b5c3.js"
+  },
+  {
+    "revision": "19134af64406142dc0b1",
+    "url": "/ecomerce-app/static/js/main.94153e7c.chunk.js"
+  },
+  {
+    "revision": "989137cb4e269968dfb9",
+    "url": "/ecomerce-app/static/js/2.1b195ae3.chunk.js"
+  },
+  {
+    "revision": "989137cb4e269968dfb9",
+    "url": "/ecomerce-app/static/css/2.d226bad7.chunk.css"
+  },
+  {
+    "revision": "792c754f7b9e19acafbce798bbf9a868",
+    "url": "/ecomerce-app/index.html"
+  }
+];
