1023484c166aca05f2f093ad7925966efd79866d
gaserd <gaserd123@gmail.com>
2019-04-02 16:19:46 +0300

    fix hash / request.id

--- src/App.js
+++ src/App.js
@@ -25,43 +25,42 @@ class App extends React.Component {
 		connect.subscribe((e) => {
 			switch (e.detail.type) {
 				case 'VKWebAppAccessTokenReceived':
-					this.setState({ authToken : e.detail.data.access_token });
+					this.setState({ authToken : e.detail.data.access_token }, function() {
+						let hash = this.getLocationHash()
+						if (hash === 'items') {
+							this.setState({ activePanel : 'items' }, function() { this.getItems() })
+						} else if (hash.indexOf('item') !== -1) {
+							let id = hash.replace('item','')
+							this.setState({ activePanel : 'item' }, function() { this.getInfoItem(id) })
+						} else {
+							this.setState({ activePanel : 'items' }, function() { this.getItems() })
+						}
+					});
 					break;
 				case 'VKWebAppCallAPIMethodResult':
-					console.log(e.detail.data)
-					if (e.detail.data.request_id === 1234567) {
+					if (e.detail.data.request_id === "items") {
 						this.setState({ items : e.detail.data.response.items })
 					}
-					if (e.detail.data.request_id === 123) {
+					if (e.detail.data.request_id === "item") {
 						this.setState({ item : e.detail.data.response.items[0] })
 					}
 					break;
 				default:
-					console.log(e.detail.type);
+					console.log(e.detail);
 			}
 		});
-		connect.send("VKWebAppGetAuthToken", {"app_id": 6918064, "scope": "market"});
-
-		let hash = this.getLocationHash()
-		if (hash === 'items') {
-			this.setState({ activePanel : 'items' }, function() { this.getItems() })
-		} else if (hash.indexOf('item') !== -1) {
-			let id = hash.replace('item','')
-			this.setState({ activePanel : 'item' }, function() { this.getInfoItem(id) })
-		} else {
-			this.setState({ activePanel : 'items' }, function() { this.getItems() })
-		}
+		connect.send("VKWebAppGetAuthToken", {"app_id": 6906999, "scope": "market"});
 	}
 
 	getItems() {
 		const ownerId = -124527492
-		connect.send("VKWebAppCallAPIMethod", {"method": "market.get", "request_id" : 1234567, "params": {"owner_id": ownerId , "v":"5.92", "access_token": this.state.authToken }});
+		connect.send("VKWebAppCallAPIMethod", {"method": "market.get", "request_id" : "items", "params": {"owner_id": ownerId , "v":"5.92", "access_token": this.state.authToken }});
 	}
 
 	getInfoItem(id) {
 		const ownerId = -124527492
 		let itemId = `${ownerId}_${id}`
-		connect.send("VKWebAppCallAPIMethod", {"method": "market.getById", "request_id" : 123,  "params": {"item_ids": itemId , "v":"5.92", "extended" : 1,  "access_token": this.state.authToken }});
+		connect.send("VKWebAppCallAPIMethod", {"method": "market.getById", "request_id" : "item",  "params": {"item_ids": itemId , "v":"5.92", "extended" : 1,  "access_token": this.state.authToken }});
 	}
 
 	go = (e, object) => {
